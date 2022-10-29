ca4ab59d0227dad8a649592e5aab863c35a546de
Anas jaidi <anas.jaidi@icloud.com>
2022-10-29 13:38:11 +0100

    chore: finish all obj types

---
+++ learn-ts/objects.js
@@ -0,0 +1,10 @@
+var obj = {
+    name: 'anasjaidi',
+    age: 19
+};
+var arr = ['anas jaidi', 'mqwed'];
+var arr2 = ['anas jaidi', 'mqwed', 12];
+var ca = 12;
+var tuple = [19, "anas jaidi"];
+tuple.push(12);
+console.log(tuple);
--- learn-ts/objects.ts
+++ learn-ts/objects.ts
@@ -5,3 +5,17 @@ const obj : {
   name : 'anasjaidi',
   age :  19
 }
+
+const arr : string[] = ['anas jaidi', 'mqwed']
+const arr2 : (string | number)[] = ['anas jaidi', 'mqwed', 12]
+
+const ca :  12 = 12
+
+const tuple : [number, string] = [19, "anas jaidi"]
+tuple.push(12) // pushing to tuples with fixed siare valid 
+
+
+enum An { // dot notation
+  v1,
+  v2
+}

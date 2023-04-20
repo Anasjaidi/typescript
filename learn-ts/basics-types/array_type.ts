283a79176ffe301b2c48b9d0bae817173b9b7bc8
Anas jaidi <anas.jaidi@icloud.com>
2023-04-20 14:53:40 +0000

    getting start with arrays

---
+++ learn-ts/basics-types/array_type.ts
@@ -0,0 +1,20 @@
+const person3 = {
+	name: "new",
+	age: 12,
+	hobbies: ["coding"], // hobbies: string[]
+
+};
+
+// const my_arr: string[]
+const my_arr = ["anas"]
+
+let new_arr: string[] = [];
+
+new_arr.push("anas")
+
+for (const str of new_arr) {
+  console.log(str.substring(0, 5));
+  //! console.log(str.map()); ERROR !!!!!!
+  
+  
+}

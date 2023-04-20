8c08a561e3ad140ebd928de202dc4e9ff0f289a4
Anas jaidi <anas.jaidi@icloud.com>
2023-04-20 14:07:40 +0000

    add basic types

---
+++ learn-ts/basics-types/core_types.ts
@@ -0,0 +1,15 @@
+// numbers : floats + int
+// string : chars + strings
+// boolean
+
+import { log } from "console";
+
+
+
+function testTypes(a1 : number , a2 : boolean , a3 : string) {
+  return [a1, a2, a3]
+}
+
+const arr: (number| boolean| string)[] = testTypes(12, true, "anas");
+
+log(arr)

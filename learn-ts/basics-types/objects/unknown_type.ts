eaa3068c80e1bdbc3bfae242f42eeadead8276fa
Anas jaidi <anas.jaidi@icloud.com>
2023-04-20 17:07:09 +0000

    unknown type

---
+++ learn-ts/basics-types/objects/unknown_type.ts
@@ -0,0 +1,14 @@
+let testVar: unknown;
+let testVar2: string;
+
+testVar = 12;
+testVar = "anas jaidi";
+testVar = false;
+
+if (typeof testVar === 'string')
+  testVar2 = testVar
+
+// testVar2 = testVar // ERROR !!!!!!!
+
+
+

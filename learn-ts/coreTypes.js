76f25fb445b3aa38e76e730f8e66c94f12278f0c
Anas jaidi <anas.jaidi@icloud.com>
2023-04-20 13:40:23 +0000

    clean setup

--- learn-ts/coreTypes.js
+++
@@ -1,9 +0,0 @@
-var add = function (a, b, str, print) {
-    if (print)
-        console.log(str + (a + b));
-    else
-        console.log(a + b);
-};
-var a = 1;
-var b = 2;
-add(a, b, 'result is ', true);

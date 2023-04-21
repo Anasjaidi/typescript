c45c60bf6089fd9f2d72393dd95d3f23b867818f
Anas jaidi <anas.jaidi@icloud.com>
2023-04-21 17:10:41 +0000

    clean

--- learn-ts/generics/other_gen.js
+++
@@ -1,8 +0,0 @@
-var countAndDesc = function (a) {
-    var desc = "Non";
-    if (a.length) {
-        desc = 'Got ' + a.length;
-    }
-    return [a, desc];
-};
-console.log(countAndDesc("hello there"));
